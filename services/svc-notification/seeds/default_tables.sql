15102d99b95c17feafbe2b7e775acc30399c4d8e
lzf <lzf@lzfs-MacBook-Air.local>
2025-07-08 05:10:39 +0700

    adjustment ddl seeds notification & db port

--- services/svc-notification/seeds/default_tables.sql
+++ services/svc-notification/seeds/default_tables.sql
@@ -16,17 +16,3 @@ CREATE TABLE IF NOT EXISTS email_logs (
 CREATE INDEX IF NOT EXISTS idx_email_logs_to_email ON email_logs(to_email);
 CREATE INDEX IF NOT EXISTS idx_email_logs_status ON email_logs(status);
 CREATE INDEX IF NOT EXISTS idx_email_logs_created_at ON email_logs(created_at);
-
--- Create function to update updated_at timestamp
-CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.updated_at = CURRENT_TIMESTAMP;
-    RETURN NEW;
-END;
-$$ language 'plpgsql';
-
--- Create trigger to automatically update updated_at
-CREATE TRIGGER update_email_logs_updated_at 
-    BEFORE UPDATE ON email_logs 
-    FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
